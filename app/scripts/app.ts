466c65c8a1482a020e934765b47f2a26e0718070
Paweł Kornijów <pawel.kornijow@blstream.com>
2016-04-13 21:10:22 +0200

    get fid of unnecessary module definition

--- app/scripts/app.ts
+++ app/scripts/app.ts
@@ -2,7 +2,6 @@
 require("./modules/main/module");
 require("./modules/moduleName/module");
 require("./modules/es6features/module");
-//require("./modules/logger/module");
 
-// import {ES6} from './modules/es6features/module';
+
 
--- app/scripts/modules/es6features/module.ts
+++ app/scripts/modules/es6features/module.ts
@@ -2,13 +2,11 @@ import {ES6featuresDescriptor} from "./classes/ES6featuresDescriptor"
 import {FeatureFactory} from "./classes/FeatureFactory";
 import {ConsoleLogger, ScreenLogger} from "../logger/module"
 
-export module ES6
-{
-  let descriptor:ES6featuresDescriptor = new ES6featuresDescriptor(
-    new FeatureFactory(),
-    new ScreenLogger()
-  )
-  descriptor.list();
-}
+let descriptor:ES6featuresDescriptor = new ES6featuresDescriptor(
+  new FeatureFactory(),
+  new ScreenLogger()
+)
+descriptor.list();
+
 
 
